867c366faf41e72d0dc3439113a70d84a759334a
Liz Chapman <Lizjdavenport@gmail.com>
2017-12-06 20:19:44 -0500

    main controller using factory, updating variable name

--- maincontroller.js
+++ maincontroller.js
@@ -1,9 +1,9 @@
 var app = angular.module("myApp");
 
 app.controller('maincontroller', function($scope, $http, Factory, $location) {
-$http.get('https://api.foursquare.com/v2/venues/search?client_id=ERCUS3MJIAMB0DFCMDQIZCRUMCA0RA01JM5L2BSKZEQ5FLLB&client_secret=ADOMAVTDRQDKWBB2OATWD4FLGEGXOTU5QC0ZD4DVG3DMR5I4&v=20150609&ll=42.3314,-83.0458').then(function(response){
-$scope.post = response.data.response.venues;
-console.log($scope.post)
+Factory.getPosts().then(function(response){
+$scope.venues = response;
+console.log($scope.venues);
 }); 
     
 $scope.searchArea = function(){
