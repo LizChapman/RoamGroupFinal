dd1d045401288d781c9f7df41b459f69019a4022
Curtis Cooper <curtiscooper22@gmail.com>
2017-12-07 20:02:01 -0500

    adding twitter factory and new server into it

---
+++ public/twitterFactory.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp');
+app.factory('TwitterFactory', function($http, geoFactory){
+    return {
+        getTweets
+    };
+
+    function getTweets () {
+        var search = 'detroit';
+        var headers ={ 
+            authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAArR3QAAAAAAqFlSTW6HsfIhjWONcJBu%2FeDkxKc%3DqraFuk6WVbzQh1gWofY2XJewHa9nwEIYKHTvzAHJBEDwwKpS7n'
+        };
+    
+        return geoFactory.getLocation().then(function(location){
+           return $http.get(`api/twitter/search/tweets.json?q=${search}&geocode=${location.latitude},${location.longitude},1mi`, {headers}).
+                then(function(response) {
+                    return response.data.statuses;        
+                });
+
+        });
+        
+    }
+});
